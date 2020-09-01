67fa1645a9e396ba592978496fe18c166b066ebb
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-09-01 17:42:16 +1000

    first commit

---
+++ src/main/java/com/minutes/jpa/hibernate/demoJUnit/DemoJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.minutes.jpa.hibernate.demoJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minutes/jpa/hibernate/demoJUnit/DemoJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minutes.jpa.hibernate.demoJUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
